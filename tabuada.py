058389ee221769b23091456befce559b04c3e740
Andre Lui Tornice <65464498+Aussk@users.noreply.github.com>
2022-05-26 00:49:40 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,8 @@
+v = int(input('digite um valor inteiro: '))
+
+print(f'TABUADA DO {v} !!!!!! QUE HYPE')
+print('=' * 30)
+print(f'{v} x 1 = {v} \n{v} x 2 = {v * 2} \n{v} x 3 = {v * 3}')
+print(f'{v} x 4 = {v * 4} \n{v} x 5 = {v * 5} \n{v} x 6 = {v * 6}')
+print(f'{v} x 7 = {v * 7} \n{v} x 8 = {v * 8} \n{v} x 9 = {v * 9}')
+print(f'{v} x 10 = {v * 10}')
